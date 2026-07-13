ef08655991150082ca330a2d424932394865d70d
rastrocchia46 <ra.strocchia@gmail.com>
2026-07-13 18:56:40 +0200

    Delete site-config.js

--- site-config.js
+++
@@ -1,7 +0,0 @@
-window.TORNEO_CONFIG = {
-  // Incolla qui l'URL /exec ottenuto pubblicando lo script come Web app.
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzbicyHiUyK9PwtE5yniniH80eO6q6q6Kvl_5m82sGJaeCz8Ee_SaVUTsZrx0w7V6k/exec",
-
-  // Il sito richiede nuovamente i dati ogni 60 secondi.
-  refreshMs: 60000
-};
